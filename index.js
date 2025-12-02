80a28a545d375a4984c5979149eebab9d19ebc02
madeforlosers <87035377+madeforlosers@users.noreply.github.com>
2025-12-02 18:59:52 +0000

    got rid of comparison operators in favor of more functions (its more stable that way anyway) and fixed examples accordingly

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ const { getSystemErrorMap } = require("util");
 const prompt = require("prompt-sync")();
 const splitCom = (s, item = ",") => [...s].map(c => c == item & !(a += ("{[()]}".indexOf(c) + 4) % 7 - 3) ? `
 `: c, a = 0).join``
-function splitExpr(item) {
-    // console.log(item)
-    return splitCom(splitCom(splitCom(splitCom(item, "<"), ">"), "!"), "=")
-
-}
 
 var tape = [];
 var functionC = [];
@@ -21,11 +16,32 @@ var funcs = {
     "set": function (index, item) {
         tape[parseInt(index)] = item;
     },
+    "increment": function (index) {
+        tape[parseInt(index)]++;
+    },
+    "decrement":function(index){
+        tape[parseInt(index)]--;
+    },
     "ask": function (text) {
         return prompt(text);
     },
     "add": function (number1, number2) {
         return number1 + number2
+    },
+    "more": function (number1, number2) {
+        return number1 > number2
+    },
+    "less": function (number1, number2) {
+        return number1 < number2
+    },
+    "moreis": function (number1, number2) {
+        return number1 >= number2
+    },
+    "lessis": function (number1, number2) {
+        return number1 <= number2
+    },
+    "is": function (number1, number2) {
+        return number1 == number2
     },
     "subtract": function (number1, number2) {
         return number1 - number2
@@ -42,6 +58,9 @@ var funcs = {
     "xor": function (number1, number2) {
         return number1 ^ number2
     },
+    "not": function (number) {
+        return !number
+    },
     "modulo": function (number1, number2) {
         return number1 % number2
     },
@@ -56,6 +75,9 @@ var funcs = {
     },
     "toInt": function (number) {
         return parseInt(number)
+    },
+    "fpart": function (number) {
+        return number - parseInt(number);
     },
     "get": function (index) {
         return tape[parseInt(index)]
@@ -193,13 +215,14 @@ function runCommands(input, nested = false) {
         // console.log(input.replace(/"/g,""))
         return input.replace(/"/g, "");
     }
+    /*
     if (nested) {
         // console.log(splitExpr(input).split("\n"))
         if (splitExpr(input).split("\n").length > 1) {
             // console.log("!!!"+input)
             return runQuery(input)
         }
-    }
+    }*/
     if (input.match(/^\w+\(.*\)+$/g) != null) {
         return runFunc(input);
     }
@@ -215,12 +238,6 @@ function runCommands(input, nested = false) {
 
 }
 
-function runQuery(input) {
-    regex = /(?:(?!\B"[^"]*)\s*(?:!|=|>|<)\s*(?![^"]*"\B))(?![^()]*(?:\([^()]*\))?\))/g
-    h = splitExpr(input).split("\n")
-    // console.log(input.match(regex)[0])
-    return eval(runCommands(h[0].trim(), true) + "" + ["!=", "==", ">", "<"]["!=><".indexOf(input.match(regex)[0].trim())] + "" + runCommands(h[1].trim(), true))
-}
 function runFunc(input) {
     let getFunc = input.split("(")[0];
 
